1537eb496e8707cb082f59ff2563ca83c1393aac
Arthur <Arthur1606@github.com>
2026-04-12 22:54:41 -0500

    Hotfix: Restore DOMContentLoaded wrapper in main.js

--- public/main.js
+++ public/main.js
@@ -1,3 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
     // Mobile Menu Toggle
     const menuToggle = document.querySelector('.menu-toggle');
     const navLinks = document.querySelector('.nav-links');
